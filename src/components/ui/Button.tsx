4ac2a7f4eb76406d74478e1d1994a342692fa1f1
Tyler Nickerson <nickersoft@gmail.com>
2025-11-25 11:20:02 -0500

    address feedback

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -10,15 +10,15 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground ring-4 ring-primary/30 shadow-md hover:bg-primary/95 hover:ring-6 ease-out",
+          "bg-sky-700 text-sky-50 ring-4 ring-sky-600/30 dark:ring-sky-300/30 shadow-md hover:bg-sky-600 hover:ring-6 ease-out",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-primary/8 text-muted-foreground hover:text-primary ",
-        link: "text-primary underline-offset-4 hover:underline",
+        ghost: "hover:bg-sky-700/8 text-muted-foreground hover:text-sky-700 ",
+        link: "text-sky-700 underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
